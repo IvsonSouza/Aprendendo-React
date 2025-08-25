f649afc2e192d0596308d7bbe779cbc7edd1112c
IvsonSouza <ivsonsouzadev@gmail.com>
2025-08-25 16:51:23 -0300

    Ajuste check

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ChevronRight, Trash } from "lucide-react";
+import { CheckCheck, CheckIcon, ChevronRight, Trash } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button.jsx";
 
@@ -19,10 +19,11 @@ function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
         <li key={task.id} className="flex gap-2">
           <button
             onClick={() => onTaskClick(task.id)}
-            className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${
+            className={`bg-slate-400 text-left w-full flex items-center gap-2 text-white p-2 rounded-md ${
               task.isCompleted && "line-through"
             }`}
           >
+            {task.isCompleted && <CheckIcon}
             {task.title}
           </button>
           <Button
